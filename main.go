8a3427caf06269b16c2953978776a6e9807c92f7
smallbraintime <patrykstruzek@proton.me>
2025-02-28 21:45:13 +0100

    fix downloading

--- main.go
+++ main.go
@@ -2,25 +2,16 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type Download struct {
 	url, path string
-}
-
-func getArgs() (Download, error) {
-	args := os.Args
-	if len(args) > 3 || len(args) < 2 {
-		return Download{}, errors.New("Invalid Arguments")
-	}
-	return Download{
-		args[1],
-		args[2],
-	}, nil
 }
 
 func download(download *Download) error {
@@ -30,25 +21,53 @@ func download(download *Download) error {
 	}
 	defer resp.Body.Close()
 
-	file, err := os.Create(download.path)
-	if err != nil {
-		return err
+	index := strings.LastIndex(download.url, "/")
+	if index == -1 {
+		return errors.New("invalid url")
 	}
+
+	var file *os.File
 	defer file.Close()
+	if download.path != "" && download.path != "." {
+		file, err = os.Create(download.path + download.url[index:])
+		if err != nil {
+			return err
+		}
+	} else {
+		currentDir, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+
+		file, err = os.Create(currentDir + download.url[index:])
+		if err != nil {
+			return err
+		}
+	}
 
 	_, err = io.Copy(file, resp.Body)
 	return err
 }
 
 func main() {
-	arg, err := getArgs()
-	if err != nil {
-		log.Println(err)
+	args := os.Args
+	if len(args) > 3 || len(args) == 2 {
+		log.Fatal(errors.New("Invalid Arguments"))
 	}
 
-	_, err = os.Stat(arg.path)
+	if len(args) < 2 {
+		fmt.Printf("daccel 0.1\nUsage: daccel <url> <dest>")
+		return
+	}
+
+	arg := Download{
+		args[1],
+		args[2],
+	}
+
+	_, err := os.Stat(arg.path)
 	if err != nil {
-		log.Println(errors.New("couldn't find given path"))
+		log.Fatal(errors.New("couldn't find given path"))
 	}
 
 	err = download(&arg)
